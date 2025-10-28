b45a8980776fbdd72fada890666df6bf5796479f
Stein Arild <103763064+Arkuradev@users.noreply.github.com>
2025-10-28 20:09:59 +0100

    First initial push - Setting up project with Vite/React + Tailwind v3

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen grid place-items-center bg-gray-100">
+      <h1 className="text-3xl md:text-5xl font-bold text-blue-600">
+        Tailwind v3 is alive! 🎉
+      </h1>
+    </div>
+  );
+}
+
+
