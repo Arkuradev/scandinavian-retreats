524aa147ab6bb9b402ecfa0d124bb00b1ede7423
Stein Arild <103763064+Arkuradev@users.noreply.github.com>
2025-11-23 00:03:16 +0100

    Bug fixing of filename changed

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import Layout from "@/components/layout/Layout";
 import Home from "@/pages/Home.tsx";
 import Venues from "@/pages/Venues";
 import CreateVenuePage from "@/pages/CreateVenuePage";
-import MyBookingsPage from "@/pages/MyBookingsPage";
+import MyBookingsPage from "@/pages/myBookingsPage";
 import ManageVenuesPage from "@/pages/ManageVenuesPage";
 import Account from "@/pages/Account.tsx";
 import NotFound from "@/pages/NotFound.tsx";
